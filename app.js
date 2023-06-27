97344e5b127c9d1663ce8eeeb85a599f731645e0
Sunday Morenikeji <65197762+areel007@users.noreply.github.com>
2023-06-27 13:06:30 +0100

    created endpoints for application form submission

--- app.js
+++ app.js
@@ -17,12 +17,14 @@ const authRoute = require("./routes/auth");
 const newsRoute = require("./routes/news");
 const areaRegisterRoute = require("./routes/area.register");
 const jobVacancyRoute = require("./routes/job.vacancy");
+const applicationFormRoute = require("./routes/application.form");
 const testRoute = require("./routes/test");
 
 app.use("/api/v1/auth", validate, authRoute);
 app.use("/api/v1/news", newsRoute);
 app.use("/api/v1/area-register", areaRegisterRoute);
 app.use("/api/v1/jobs", jobVacancyRoute);
+app.use("/api/v1/application", applicationFormRoute);
 app.use("/api/v1/test", testRoute);
 
 module.exports = app;
